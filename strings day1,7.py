75e88c603663236ba2af103bac12c05f9de84372
Syedakram7 <122328840+Syedakram7@users.noreply.github.com>
2023-02-13 09:02:23 +0530

    Create strings day1,7.py

---
+++ strings day1,7.py
@@ -0,0 +1,20 @@
+def countstrings(n,start):
+    if n==0:
+        return 1
+      cnt = 0
+      fori in range(start,5):
+        cnt+=countstrings(n-1,i)
+        return cnt
+def countvowelsstrings(n):
+  return countstrings(n,0)
+n=int(input("enter the n value"))
+print(countvowelstrings(n))
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Con
